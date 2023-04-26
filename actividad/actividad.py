1684b98218f008ce1662dc4ff8868d860d57b248
Brayan <brayansandoval556@gmail.com>
2023-04-26 08:58:43 -0500

    Ejemplos de bulces, condicionales

--- actividad/actividad.py
+++ actividad/actividad.py
@@ -1,1 +1,16 @@
-print ("hola brayan")
+i=1
+sum=0
+par=2
+inp=1
+while i<=10:
+    print(i)
+    sum+=i
+    i+=1
+    print("la suma de todos es: ",sum)
+
+    par+=2 
+    inp+=2
+    print("los impares son:",inp) 
+    print("la pares son:",par)
+  
+    inp=inp+2
